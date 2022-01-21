73afc06053de3c0bee4fcadf721d659d0d7c43ef
신동규 <donggyu9410@gmail.com>
2022-01-22 00:26:57 +0900

    Make DGZoomableImageView

--- Sources/DGZoomableImageView/DGZoomableImageView.swift
+++ Sources/DGZoomableImageView/DGZoomableImageView.swift
@@ -1,6 +1,44 @@
-public struct DGZoomableImageView {
-    public private(set) var text = "Hello, World!"
+import UIKit
+open class DGZoomableImageView: UIScrollView {
+    
+    private let imageView = UIImageView()
+    public var image: UIImage? {
+        didSet {
+            guard let image = image else { return }
+            self.imageView.image = image
+        }
+    }
+    
+    convenience init(image: UIImage) {
+        self.init(frame: .zero)
+        self.image = image
+        configureUI()
+    }
+    
+    private func configureUI() {
+        // Setup image view
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.widthAnchor.constraint(equalTo: widthAnchor),
+            imageView.heightAnchor.constraint(equalTo: heightAnchor),
+            imageView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: centerYAnchor)
+        ])
+        
+        // Setup scroll view
+        minimumZoomScale = 1
+        maximumZoomScale = 3
+        showsHorizontalScrollIndicator = false
+        showsVerticalScrollIndicator = false
+        delegate = self
+    }
+    
+}
 
-    public init() {
+extension DGZoomableImageView: UIScrollViewDelegate {
+    public func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return imageView
     }
 }
